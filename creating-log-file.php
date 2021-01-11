2af02ff1843681e3024c3596ee5710a56eb6b671
Abhishek Gupta <abhishek.pec2015@gmail.com>
2021-01-10 21:21:07 -0500

    Create creating-log-file.php

---
+++ creating-log-file.php
@@ -0,0 +1,7 @@
+//Something to write to txt log
+$log  = "User: ".$_SERVER['REMOTE_ADDR'].' - '.date("F j, Y, g:i a").PHP_EOL.
+        "Attempt: ".($result[0]['success']=='1'?'Success':'Failed').PHP_EOL.
+        "User: ".$username.PHP_EOL.
+        "-------------------------".PHP_EOL;
+//Save string to log, use FILE_APPEND to append.
+file_put_contents('./log_'.date("j.n.Y").'.log', $log, FILE_APPEND);
